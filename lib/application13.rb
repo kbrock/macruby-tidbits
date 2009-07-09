329e01b42a7bb28dfcab8dd63f39be1f89868345
Keenan Brock <keenan@thebrocks.net>
2009-07-09 04:09:56 -0400

    fixed variable column example

--- lib/application13.rb
+++ lib/application13.rb
@@ -9,35 +9,31 @@ class Application
       window(:title => "MyTableView",
         :frame => [10, 620, 330, 230]) do |win|
         win << split_view(:horizontal => true,
-          :layout => FULL) do |sv|
-          sv << scroll_view(:frame => SHOW) do |sc|
-            @num_cols=1
-            sc << @table=table_view(
-              :data => [], :columns => [
-                column(:id => :c1, :title => "Word 1")])
-          end #sc
-          sv << @prompt = text_field(:text => 'type here',
+          :layout => FULL,:frame=>SHOW) do |sp|
+          sp << sc=scroll_view(:frame => SHOW)
+          sc << @tv=table_view(:data=>[],:columns=>[])
+          sp << @pr=text_field(:text => 'type here',
             :font => font(:name=>'Monaco', :size => 16),
             :on_action => Proc.new {|t| type(t.to_s)})
-        end #sv
+        end #sp
         win.contentView.margin = 5
-        win.makeFirstResponder @prompt
+        win.makeFirstResponder @pr
         win.will_close { exit }
       end #window (win)
     end #application
   end
   def type(t)
-    c=t.split
-    #add extra columns
-    ((@num_cols+1)..c.size)).each do |i|
-      @table.dataSource.columns <<
-        column(:id => "c#{i}", :title => "Word #{i}")
-    end
     values={}
-    c.each_with_index do |v,i| values["c#{i}".to_sym]=v end
-    @table.dataSource.data << values
-    @table.reloadData
+    t.split.each_with_index do |v,i|
+      values["c#{i}"]=v
+      if i >= @tv.tableColumns.size
+        @tv.tableColumns << column(:id=>"c#{i}",
+          :title=>"Word #{i+1}")
+      end
+    end
+    @tv.dataSource.data << values
+    @tv.reloadData
   end
 end
-a=Application.new.start
+Application.new.start
 #thanks http://everburning.com/news/heating-up-with-hotcocoa-part-ii/
